28c0aec28359ed434d06d37b3babbad84518f4d7
ausilianapoli <ausilianapoli@gmail.com>
2019-04-24 22:07:11 +0200

    comment useless global variable

--- AudioManaging.py
+++ AudioManaging.py
@@ -14,7 +14,7 @@ import platform
 class AudioManaging:
     
     audio_file = [] #list of tuple(path, samplerate, data)
-    audio_numpy = [] #list of tuple(signal) after ifft
+    #audio_numpy = [] #list of tuple(signal) after ifft
     
     #It allows to read the file audio.wav from path
     def read_file(self, path):
