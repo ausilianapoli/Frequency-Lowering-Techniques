da916ed0bed1808e74545b9d5d39a46e7ebf5ef7
ausilianapoli <ausilianapoli@gmail.com>
2019-05-08 21:45:39 +0200

    add high pass filter application

--- Comparator.py
+++ Comparator.py
@@ -20,11 +20,14 @@ number = 12 #or 4
 am = AudioManaging()
 am_lp = AudioManaging() #low pass
 am_ct = AudioManaging() #compression technique
+am_hp = AudioManaging() #high pass
 ft_lp = FourierTransform() #low pass
 ft_ct = FourierTransform() #compression technique
+ft_hp = FourierTransform() #high pass
 gr = Graphs()
 fc_lp = FrequencyCompression(low_cutoff, high_cutoff, ratio, CR, samplerate) #low pass
 fc_ct = FrequencyCompression(low_cutoff, high_cutoff, ratio, CR, samplerate) #compression technique
+fc_hp = FrequencyCompression(low_cutoff, high_cutoff, ratio, CR, samplerate) #high pass
 
 for i in range (1, number):
 #1 - Take path
@@ -41,7 +44,7 @@ for i in range (1, number):
 #6a - Save new wav file with low pass filter
     signal = am.convert_numpy(ft_lp.audio_ifft[i-1])
     am.save_file("testing/{}{}/{}lp".format(name, i,name), i, am.audio_file[i-1][1], signal.T)
-#3a - Time to Frequency domain for compression technique
+#3b - Time to Frequency domain for compression technique
     ft_ct.time_to_frequency(am.audio_file[i-1])
 #4b - Applying compression technique
     fc_ct.technique_2(ft_ct.audio_fft[i-1])
@@ -49,12 +52,22 @@ for i in range (1, number):
     ft_ct.frequency_to_time(fc_ct.audio_fc[i-1])
 #6b - Save new wav file with compression technique
     signal = am.convert_numpy(ft_ct.audio_ifft[i-1])
-    am.save_file("testing/{}{}/{}ct".format(name, i, name), i, am.audio_file[i-1][1], signal.T)  
-#7 - Print metadata and read new wav files
-    #am.print_metadata(path)
+    am.save_file("testing/{}{}/{}ct".format(name, i, name), i, am.audio_file[i-1][1], signal.T)
+#3c - Time to Frequency domain for high pass filter
+    ft_hp.time_to_frequency(am.audio_file[i-1])
+#4c - Applying high pass filter
+    fc_hp.applyHPGaussian(ft_hp.audio_fft[i-1])
+#5c - Frequency to Time domain for high pass filter
+    ft_hp.frequency_to_time(fc_hp.audio_fc[i-1])
+#6c - Save new wav file with high pass filter
+    signal = am.convert_numpy(ft_hp.audio_ifft[i-1])
+    am.save_file("testing/{}{}/{}hp".format(name, i,name), i, am.audio_file[i-1][1], signal.T)    
+#7 - Read new wav files
     am_lp.read_file("./records/testing/{:s}{:d}/{:s}lp_{:d}.wav"\
                         .format(name, i, name, i))
     am_ct.read_file("./records/testing/{:s}{:d}/{:s}ct_{:d}.wav"\
+                        .format(name, i, name, i))
+    am_hp.read_file("./records/testing/{:s}{:d}/{:s}hp_{:d}.wav"\
                         .format(name, i, name, i))
 #8 - Plot waveform and spectrogram
     gr.waveform(am.audio_file[i-1])
